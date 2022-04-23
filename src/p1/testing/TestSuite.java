58c6928083d2ad3a5d28c5e6202a26801dfa2933
Jacob Bennett <57199778+Sarsaparilla29@users.noreply.github.com>
2022-04-22 22:37:58 -0400

    3 unit tests completed

---
+++ src/p1/testing/TestSuite.java
@@ -0,0 +1,10 @@
+package p1.testing;
+
+/*import pool.plugins.org.junit.platform.suite.api_1.8.1.v20211018-1956.jar;
+
+@Suite
+@SuiteDisplayName("Suite combining all the tests")
+@SelectClasses({p1.ClockTest.class})
+public class TestSuite {
+
+}*/
