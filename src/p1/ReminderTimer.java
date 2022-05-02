613fe3aa6878e42af365d300fe4e8062149cd61d
Jacob Bennett <57199778+Sarsaparilla29@users.noreply.github.com>
2022-05-02 16:19:47 -0400

    Added methods to get the current time

--- src/p1/ReminderTimer.java
+++ src/p1/ReminderTimer.java
@@ -1,9 +1,30 @@
 package p1;
 
 import java.util.Timer;
+import java.util.Scanner;
+
 
 public class ReminderTimer {
 	
+	int currentTime = 0;
 	
-
+	long getTime() {
+		long currentTime = System.currentTimeMillis();
+		return currentTime;
+	}
+	
+	long alarmTime(long currentTime, long addedTime) {
+		long alarm = currentTime + addedTime;
+		return alarm;
+	}
+	
+	long calcAddedTime() {
+		long currentTime = getTime();
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter Time: ");
+		long alarmTime = input.nextLong();
+		long addedTime = alarmTime - currentTime;
+		return addedTime;
+	}
+	
 }
