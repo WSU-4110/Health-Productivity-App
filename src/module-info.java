b73a0a40259d492e0fb172f34e8092074e9e42b1
Jacob Bennett <57199778+Sarsaparilla29@users.noreply.github.com>
2022-05-02 16:14:57 -0400

    fixed with local version

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,5 @@
 module healthapp {
 	requires java.desktop;
 	requires java.sql;
-	requires org.junit.jupiter.api;
-<<<<<<< HEAD
-	requires org.opentest4j;
-=======
->>>>>>> b847d6774741433e4a7e5f26ac543619c9327630
+	//requires org.junit.jupiter.api;
 }
